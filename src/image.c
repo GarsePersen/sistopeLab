723229003ee64959b15bf5f91b055fc0ac9f96f6
michaelCastillo <michael.castillo.p96@gmail.com>
2018-04-01 18:04:08 -0300

    Recorrido exitoso, almacena F0 y FF correctamente, falta calibrar el tema de los padding.

--- src/image.c
+++ src/image.c
@@ -19,8 +19,10 @@ Image *loadImage(char *file_name){
         //Se valida que el archivo sea una imagen en BMP
         char b;
         char m;
+        int count = 0;
         read(id_open,&b,sizeof(char));
         read(id_open,&m,sizeof(char));
+        count += 2;
         if(b != 'B' || m != 'M'){
             printf("No es un archivo BMP\n");
             return NULL;
@@ -33,14 +35,21 @@ Image *loadImage(char *file_name){
             
             //Proceso de extracción del puntero de los datos (bit 10)
             read(id_open,&temp,8);
+            count += 8;
             read(id_open,&(img->dataPointer),4);
+            count += 4;
             //Proceso de lectura de dimensiones (bit 18)
             read(id_open,&temp,4);
+            count += 4;
             read(id_open,&img->width,4); //ancho.
+            count += 4;
             read(id_open,&img->height,4); //alto.
+            count += 4;
             //Proceso de extracción de variable de compresión (bit 30);
             read(id_open,&temp,4);
+            count += 4;
             read(id_open,&img->isCompressed,4); //está comprimido?
+            count += 4;
 
             if(img->isCompressed != 0){
                 printf("Está comprimido.\n");
@@ -51,32 +60,53 @@ Image *loadImage(char *file_name){
 
             printf("Dp: %i\n",img->dataPointer);
             printf("ancho: %i\n",img->width);
-            printf("Largo: %i\n",img->height);
+            printf("Altp: %i\n",img->height);
             printf("está comprimido?: %i\n",img->isCompressed);
 
             //Proceso de extracción de tuplas.
             //Han pasado 26 ciclos de lectura por lo tanto 
             int i = 0;
-            for(x=0; x<(img->dataPointer - 26); x++){
+            printf("dp - 26: %i\n",img->dataPointer - 26);
+            printf("count: %i\n",count);
+            for(x=0; x<(img->dataPointer - count); x++){
                 i = read(id_open,&temp,1);
             }
+
             
             printf("i: %i\n",i);
             int y;
-            img->triads = (Triad**)malloc(sizeof(Triad*)*img->height);
+            //Denuevo
+            img->triads = (Triad**)malloc(sizeof(Triad*)*img->height);        
             for(y = 0; y<(img->height); y++){
                 img->triads[y] = (Triad*)malloc(sizeof(Triad)*img->width);
             }
-            for(x=0; x<(img->height); x++){
-                for(y = 0; y<(img->width); y++){
+            
+            for(x= img->height - 1; x>=0; x--){
+                for(y = 0; y<img->width; y++){
                     read(id_open,&img->triads[x][y].r,1);
                     //printf("r: %i ",img->triads[x][y].r);
                     read(id_open,&img->triads[x][y].g,1);
                     //printf("g: %i ",img->triads[x][y].g);
                     read(id_open,&img->triads[x][y].b,1);
                     //printf("b: %i \n",img->triads[x][y].b);
+                    if(img->width -1 == y){
+                        read(id_open,&temp,4);
+
+                    }
                 }
             }
+            printf("pixel[0,0]: (%i,%i,%i)\n",img->triads[0][0].r,img->triads[0][0].g,img->triads[0][0].b);
+            printf("pixel[F,0]: (%i,%i,%i)\n",img->triads[img->height-1][0].r,img->triads[img->height-1][0].g,img->triads[img->height-1][0].b);
+            printf("pixel[0,F]: (%i,%i,%i)\n",img->triads[0][img->width-1].r,img->triads[0][img->width-1].g,img->triads[0][img->width-1].b);
+            printf("pixel[F,F]: (%i,%i,%i)\n",img->triads[img->height-1][img->width-1].r,img->triads[img->height-1][img->width-1].g,img->triads[img->height-1][img->width-1].b);
+            
+           int id = 0;
+            read(id_open,&id,1);
+            printf("r: %i\n",id);
+            read(id_open,&id,1);
+            printf("g: %i\n",id);
+            read(id_open,&id,1);
+            printf("b: %i\n",id);
 
             return img;
         }
