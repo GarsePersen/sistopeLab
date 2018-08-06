e34b164503aa3ed50cfd6cb41f7c94c6a7134cf6
GarsePersen <sebastian.garay.p@usach.cl>
2018-08-05 20:42:53 -0400

    Falta comentar

--- src/image.h
+++ src/image.h
@@ -1,20 +1,70 @@
+#include <stdio.h>
+#include <pthread.h>
+
+//Estructura para almacenar la matriz de pixeles
+typedef struct{
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+    unsigned char a;
+    unsigned int partition_size;
+}Triad;
+//Estructura para la imagen
+typedef struct {
+    int type;
+    int type2;
+    int fileSize;
+    int reserved1;
+    int reserved2;
+    int dataPointer;
+    int headerSize;
+    int width;
+    int height;
+    int planes;
+    int tam_img;
+    int bitPerPixel;
+    int isCompressed;
+    Triad *triads;
+    FILE* filePointer;
+    int nThreads;
+    int partition_end;
+    int partition_start;
+    int numberPixelsXthread;
+    int numberBlacks;
+    pthread_mutex_t mutexNearlyBlack;
+}Image;
 
 
 
 typedef struct{
-    int r;
-    int g;
-    int b;
-}Triad;
+    pthread_t id;
+    unsigned char* pixels;
+    unsigned int start;
+    unsigned int end;
+}DataThread;
 
-typedef struct {
+typedef struct{
+    char *file_name;
+    int umbral;
+    int nearlyBlack;
+    int threads;
+    pthread_mutex_t mutexInit;
+    pthread_mutex_t mutexCalculus;
+    pthread_barrier_t barrier;
+    int aux;
+    Image* img;
+    DataThread* dataThreads;
+}DataInit;
 
-    int height;
-    int width;
-    int dataPointer;
-    int isCompressed;
-    Triad **triads;
-}Image;
+//Funciones
+void* threadsHandler(void* data_o);
+FILE *openImage(char *file_name);
+void *convertToGrayScale(Image *img, unsigned int start, unsigned int end);
+void *readImage(Image *img, FILE *file_pointer);
+void cpy_img(char *nameFile, char *nameFileOut);
+void *convertToGrayScaleHandler(void *img_o);
+void writeImage(Image *img, FILE *file_pointer);
+unsigned int searchThread(pthread_t id, DataThread *threads, int numThread);
+void binarization(Image *img, int umbral, unsigned int start, unsigned int end);
+int nearlyBlack(int numOfBlacks, int numTotal, int umbralNearlyBlack);
 
-
-Image *loadImage(char *file_name);
--- src/main.c
+++ src/main.c
@@ -2,27 +2,29 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <unistd.h>
+#include <string.h>
 #include "image.h"
+#include <pthread.h>
 
+	
 
 int main(int argc, char **argv){
 	
-	int imageNumber = -1;
-	int thresholdB = -1;
-	int thresholdC = -1;
+	int numImgs = -1;
+	int umbral = -1;
+	int umbralNearlyBlack = -1;
 	int flag = 0;
 	int aux;
-
-	while ((aux = getopt (argc, argv, "c:u:n:b")) != -1){
+    while ((aux = getopt (argc, argv, "c:u:n:b")) != -1){
 		switch (aux){
 			case 'c':
-				imageNumber = atoi(optarg);
+				numImgs = atoi(optarg);
 				break;
 			case 'u':
-				thresholdB = atoi(optarg);
+				umbral = atoi(optarg);
 				break;
 			case 'n':
-				thresholdC = atoi(optarg);
+				umbralNearlyBlack = atoi(optarg);
 				break;
 			case 'b':
 				flag = 1;
@@ -30,25 +32,111 @@ int main(int argc, char **argv){
 			default:
 				abort();
 		}
-		
+	}
+    //Comprobaciones getopt
+    if((umbral < 0 ) || (umbral > 255)){
+        printf("\nValor del umbral incorrecto, debe ser entre 0 y 255, saliendo...\n\n");
+        return -1;
+    }
+    if((umbralNearlyBlack < 0) || (umbralNearlyBlack > 100)){
+        printf("\nValor del umbral para la categoría inválido, debe ser un porcentaje entre 0 y 100\n\n");
+        return -1;
+    }
+    if(numImgs <= 0){
+        printf("\nCantidad de imágenes inválida, debe ser igual o mayor a 1\n\n");
+        return -1;
+    }
+    if((numImgs == -1) || (umbral == -1) || (umbralNearlyBlack == -1)){
+        printf("\nNo ha ingresado todas las opciones, revisar leeme\n\n");
+        return -1;
+    }
+    int threads;
+    printf("Ingrese el numero de hebras: ");
+    scanf("%i",&threads);
+	
+	int op = 0;
+	char **imageNames = (char**)malloc(sizeof(char*)*numImgs);
+	int x;
+	for(x = 0; x<numImgs; x++){
+        char str[8];
+		imageNames[x] = (char*)malloc(sizeof(char)*100);
+        char image[8] = "imagen_";
+        sprintf(str,"%d", x+1);
+        strcat(image, str);
+        
+        strcat(image, ".bmp");
+		strcpy(imageNames[x],image);
+		memset(&image[0], 0, sizeof(image));
+		memset(&str[0], 0, sizeof(str));
+        FILE *archivo = openImage(imageNames[x]); //Comprobación de que el archivo existe
+        if(archivo == NULL){
+            printf("\nError: Archivo %s no existe\n\n", imageNames[x]);
+            return -1;
+        }
+        fclose(archivo);
+        
 
 	}
-	//Después se activa. xd
-	//Comprobaciones
-	/*if ((imageNumber == -1) || (thresholdB == -1) || (thresholdC == -1)){
-		printf("Compruebe que ingreso todas las opciones (ver manual)\n");
-		return -1;
+	
+	
+	int *resultsNearlyBlack = (int*)malloc(sizeof(int)*numImgs); 
+	for(int y = 0; y<numImgs; y++){
+		DataInit *data = (DataInit*)malloc(sizeof(DataInit));
+        data->file_name = imageNames[y];
+        data->umbral = umbral;
+        data->nearlyBlack = umbralNearlyBlack;
+        data->threads = threads;
+        pthread_mutex_init(&data->mutexInit,NULL);
+        pthread_mutex_init(&data->mutexCalculus,NULL);
+        pthread_barrier_init(&data->barrier,NULL,data->threads);
+        data->aux =0;
+        Image *img = (Image*)malloc(sizeof(Image));
+        data->img = img;
+        img->numberBlacks = 0;
+        pthread_mutex_init(&img->mutexNearlyBlack,NULL);    
+        int x;
+        DataThread* dataThreads = (DataThread*)malloc(sizeof(DataThread)*threads);
+        data->dataThreads = dataThreads;
+        for(x=0;x<threads;x++){
+            pthread_create(&dataThreads[x].id,NULL,threadsHandler,(void*)data);
+        }
+        for(x=0;x<threads;x++){
+            pthread_join(data->dataThreads[x].id,NULL);
+        }
+        writeImage(data->img,data->img->filePointer);
+
+        //Identificacion de nearly black
+        resultsNearlyBlack[y] = nearlyBlack(img->numberBlacks, img->width*img->height, umbralNearlyBlack);
+        
 	}
 
-	printf("cantImagenes: %i \numbralB: %i \numbralC: %i \nbandera: %i\n", imageNumber, thresholdB, thresholdC, flag);
-	*/
-	
-	
-	
-	//Prueba de extracción de imágen.
-	printf("Inicio de prueba de extracción de imágen.");
-	Image *img = loadImage("hola.bmp");
+    if(flag == 1){
+        printf("\n|\timage\t|\t\tnearly black\t\t|\n" );
+        for(x = 0; x<numImgs; x++){
+
+            printf("|---------------|---------------------------------------|\n");
+            if(resultsNearlyBlack[x] == 1){
+                printf("|\t%s\t|\t\tyes\t\t|\n",imageNames[x]);	
+            }else{
+                printf("|\t%s\t|\t\tno \t\t|\n",imageNames[x]);
+            }
+
+
+        }	
+
+    }
+
+    
+
+    /*
+    **** SECCION PRUEBA DE HEBRAS ****
+    */
+    
+    
 
 
 	return 0;
 }
+	
+
+
