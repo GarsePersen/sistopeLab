7a7b96d09b4e170514afc07ad159558e6776db40
MichaelCastillo <michael.castillo.p96@gmail.com>
2018-08-24 11:23:03 -0300

    se agrega version con threads del productor consumidor

--- src/Main.cc
+++ src/Main.cc
@@ -1,17 +1,63 @@
 #include <iostream>
 #include "Monitor.cc"
+#include <pthread.h>
 using namespace std;
 
+#define NUM_THREADS 15
 
+void* insert(void* x);
+void* queryy(void * arg);
+void* remove(void * arg);
+BoundedBuffer buffer;
 int main()
 {
-    UpDownCounter a(0), b(2), c;
-    cout << a.up() << endl;
-    cout << a.up() << endl;
-    cout << a.up() << endl;
-    cout << b.down() << endl;
-    cout << b.down() << endl;
-    cout << b.down() << endl;
-
+    
+    pthread_t threads_insert[NUM_THREADS];
+    pthread_t threads_query[NUM_THREADS];
+    pthread_t threads_remove[NUM_THREADS];
+    buffer.insert(2);
+    cout<<buffer.query() <<endl;
+    //cout << a.query() << endl;
+    //cout << b.insert(2)<<endl;
+    int rc;
+    
+    for(int i = 0; i<NUM_THREADS; i++){
+        pthread_create(&threads_query[i], NULL, &queryy, NULL);
+    }
+    for(int i = 0; i<NUM_THREADS; i++){
+        pthread_create(&threads_remove[i], NULL, &remove, NULL);
+    }
+    for(int i = 0; i<NUM_THREADS; i++){
+        pthread_create(&threads_insert[i], NULL, &insert, (void *)i);
+    }
+    for(int x = 0; x<3; x++){
+        for(int i = 0; i<NUM_THREADS; i++){
+            if(x ==0){
+                pthread_join(threads_insert[i],NULL);
+            }else if(x==1){
+                pthread_join(threads_query[i],NULL);
+            }else if(x==2){
+                pthread_join(threads_remove[i],NULL);
+            }
+        }
+    }
+    
     return 0;
+}
+
+
+void* insert(void* x){
+    buffer.insert((long)    x);
+    cout<<"Estoy insertando"<<endl;
+}
+
+void* queryy(void *arg){
+    int a = buffer.query();
+    cout<<"Estoy tomando el valor=> "<< a <<endl;
+    
+}
+
+void* remove(void * arg){
+    int r = buffer.remove();
+    cout<<"Estoy removiendo el valor=> "<< r <<endl;
 }
--- src/Monitor.cc
+++ src/Monitor.cc
@@ -1,26 +1,29 @@
 #include <uC++.h>
-_Monitor UpDownCounter{
-    int
-        counter;
-
-  public
-      :
-    UpDownCounter() : counter(0) {
-        
-    }
-    UpDownCounter(int init) : counter(init) {
-
-    }
-    int
-    up()
-    {
-        counter += 1;
-        return counter;
-    }
-    int
-    down()
-    {
-        counter -= 1;
-        return counter;
-    }
+_Monitor
+    BoundedBuffer {
+        uCondition full, empty;
+        int front, back, count;
+        int elements[20];
+    
+    public:
+    
+    BoundedBuffer() : front(0), back(0), count(0) {}
+    _Nomutex 
+        int query() {
+            return count; }
+        void insert(int elem) {
+            if (count == 20) empty.wait();
+            elements[back] = elem;
+            back = (back+1)% 20;
+            count += 1;
+            full.signal();
+            }
+        int remove() {
+            if(count == 0) full.wait();
+            int elem = elements[front];
+            front = (front+1)%20;
+            count -= 1;
+            empty.signal();
+            return elem;
+            }
 };
